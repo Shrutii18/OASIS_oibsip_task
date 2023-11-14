e15497f00299bb0ecee81d82d8ab7864fc8f8d62
Shruti Jadhav <113288736+Shrutii18@users.noreply.github.com>
2023-11-14 16:14:10 +0530

    Add files via upload

---
+++ 𝒐𝒊𝒃𝒔𝒊𝒑_𝒕𝒂𝒔𝒌𝒏𝒐.1/landing-page-of-cake-store/script.js
@@ -0,0 +1,5 @@
+jQuery("#content1-headline1").fitText(1.0);
+jQuery("#content1-headline2").fitText(1.0);
+jQuery("#content1-headline3").fitText(1.0);
+jQuery("#content2-1").fitText(2.0);
+jQuery("#content2-2").fitText(2.0);
